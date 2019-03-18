1a791825257462e388fdeed178e61e7db426f1b7
Oscar Brink <oscbri-7@student.ltu.se>
2019-03-18 14:08:54 +0000

    GFX stuff probably done

--- simulation/main.c
+++ simulation/main.c
@@ -1,5 +1,4 @@
 #include <pthread.h>
-//#include <ncurses.h>
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
@@ -14,7 +13,9 @@
 
 #define MAXCARSONBRIDGE 5
 
-//void ncursesInit(void);
+#define STARTX 10
+#define STARTY 10
+
 void draw(char c);
 void drawBridge(void);
 void drawLights(void);
@@ -28,7 +29,6 @@ enum { BOTHRED, SOUTHBOUNDGREEN, NORTHBOUNDGREEN } lightStatus = BOTHRED;
 enum { NOCAR, CARSOUTH, CARNORTH } bridgeBuffer[MAXCARSONBRIDGE];
 
 uint64_t arrivalBuffers[2] = { 0, 0 };
-//uint64_t bridgeBuffer[5];
 
 int main(void) {
 
@@ -55,8 +55,9 @@ int main(void) {
         bridgeBuffer[i] = a ? CARSOUTH : CARNORTH;
         a = a ? 0 : 1;
     }
-
-    arrivalBuffers[SOUTHBOUND] = 6;
+    arrivalBuffers[SOUTHBOUND] = 320;
+    arrivalBuffers[NORTHBOUND] = 355;
+    //debug
 
     do {
 
@@ -71,7 +72,7 @@ int main(void) {
 
         draw(c);
     } while ( (c = getchar()) != 'q' );
-    
+
     CLEAR();
 
     /* Restore old settings */
@@ -81,39 +82,18 @@ int main(void) {
 }
 
 
-//void ncursesInit(void) {
-//
-//    initscr();
-//    start_color();
-//    init_pair(1, COLOR_RED, COLOR_BLACK);
-//    //init_pair(2, COLOR_GREEN, COLOR_YELLOW);
-//    cbreak();               // Don't wait for line-break to read buffer.
-//    noecho();               // Don't echo back typed keys.
-////  nodelay(stdscr, TRUE);  // Sets getch as non-blocking.
-//    scrollok(stdscr, TRUE); // Sets scrolling window
-//    //timeout(-1);            // Sets blocking read
-//  
-//    nonl();                 // Return key not translated to newline.
-////  intrflush(stdscr, FALSE);  TODO What does this do ???
-//    keypad(stdscr, TRUE);   // Treat function keys separately.
-//  
-//    return;
-//}
-
 
 void draw(char c) {
     drawLights();
     drawCars();
-    //DRAW_C_AT(c, (long unsigned int) 10, (long unsigned int) 20);
-    printf("\x1B[H");
+    printf("\x1B[H"); // Send cursor to home
 }
 
 void drawBridge(void) {
-    uint64_t x = 10;
-    uint64_t y = 8;
+    uint64_t x = STARTX;
+    uint64_t y = STARTY;
 
-    DRAW_S_AT("           S", x, y);
-    y += 2;
+    DRAW_S_AT("           S", x, y - 2);
     DRAW_S_AT("-----\\     /-----", x, y);
     y++;
     DRAW_S_AT("      -----      ", x, y);
@@ -125,55 +105,65 @@ void drawBridge(void) {
     DRAW_S_AT("-----/     \\-----", x, y);
     y += 2;
     DRAW_S_AT("     N", x, y);
-
 }
 
 void drawLights(void) {
     if (lightStatus == SOUTHBOUNDGREEN) {
         printf("\x1B[42m"); // set bg green
-        DRAW_S_AT(" ", (uint64_t) 21, (uint64_t) 9);
+        DRAW_S_AT(" ", (uint64_t) (STARTX + 11), (uint64_t) (STARTY - 1));
         printf("\x1B[41m"); // set bg red
-        DRAW_S_AT(" ", (uint64_t) 15, (uint64_t) 15);
+        DRAW_S_AT(" ", (uint64_t) (STARTX + 5), (uint64_t) (STARTY + 5));
     } else if (lightStatus == NORTHBOUNDGREEN) {
         printf("\x1B[41m"); // set bg red
-        DRAW_S_AT(" ", (uint64_t) 21, (uint64_t) 9);
+        DRAW_S_AT(" ", (uint64_t) (STARTX + 11), (uint64_t) (STARTY - 1));
         printf("\x1B[42m"); // set bg green
-        DRAW_S_AT(" ", (uint64_t) 15, (uint64_t) 15);
+        DRAW_S_AT(" ", (uint64_t) (STARTX + 5), (uint64_t) (STARTY + 5));
     } else {
         printf("\x1B[41m"); // set bg red
-        DRAW_S_AT(" ", (uint64_t) 21, (uint64_t) 9);
-        DRAW_S_AT(" ", (uint64_t) 15, (uint64_t) 15);
+        DRAW_S_AT(" ", (uint64_t) (STARTX + 11), (uint64_t) (STARTY - 1));
+        DRAW_S_AT(" ", (uint64_t) (STARTX + 5), (uint64_t) (STARTY + 5));
     }
 
     printf("\x1B[0m"); // Reset attributes
 }
 
 void drawCars(void) {
-    /* Cars on arrival */
+
+    /* Northbound cars arrival */
+    printf("\x1B[36m"); // set fg cyan
+    printf("\x1B[%d;%dH%5"PRIu64, STARTY + 3, 1, arrivalBuffers[NORTHBOUND]);
     if (arrivalBuffers[NORTHBOUND] > 5) {
-        printf("\x1B[36m"); // set fg cyan
-        printf("\x1B[%d;%dH%"PRIu64"%s", 13, 12, (arrivalBuffers[NORTHBOUND] - 5), "...00000"); // Reversed order y, x
+        printf("\x1B[%d;%dH%s", STARTY + 3, STARTX - 3, "...00000"); // Reversed order: y, x
     } else {
-
+        uint64_t x = STARTX + 5;
+        for (int i = 0; i < arrivalBuffers[NORTHBOUND]; i++) {
+            DRAW_C_AT('0', x--, (uint64_t) (STARTY + 3));
+        }
     }
-
+    
+    /* Southbound cars arrival */
+    printf("\x1B[35m"); // set fg magenta
+    printf("\x1B[%d;%dH%"PRIu64, STARTY + 1, STARTX + 21, arrivalBuffers[SOUTHBOUND]);
     if (arrivalBuffers[SOUTHBOUND] > 5) {
-        printf("\x1B[35m"); // set fg magenta
-        printf("\x1B[%d;%dH%s%"PRIu64, 11, 22, "00000...", (arrivalBuffers[SOUTHBOUND] - 5) ); // Reversed order y, x
+        printf("\x1B[%d;%dH%s", STARTY + 1, STARTX + 12, "00000..."); // Reversed order: y, x
     } else {
-
+        uint64_t x = STARTX + 12;
+        for (int i = 0; i < arrivalBuffers[SOUTHBOUND]; i++) {
+            DRAW_C_AT('0', x++, (uint64_t) (STARTY + 1));
+        }
     }
 
     /* Cars on bridge */
     for (int i = 0; i < MAXCARSONBRIDGE; i++) {
         if (bridgeBuffer[i] == CARSOUTH) {
             printf("\x1B[35m"); // set fg magenta
-            DRAW_S_AT("0", (uint64_t) (16 + i), (uint64_t) 12);
+            DRAW_S_AT("0", (uint64_t) (STARTX + 6 + i), (uint64_t) STARTY + 2);
         } else if (bridgeBuffer[i] == CARNORTH) {
             printf("\x1B[36m"); // set fg cyan
-            DRAW_S_AT("0", (uint64_t) (16 + i), (uint64_t) 12);
+            DRAW_S_AT("0", (uint64_t) (STARTX + 6 + i), (uint64_t) STARTY + 2);
         }
     }
     
     printf("\x1B[0m"); // Reset attributes
 }
+
